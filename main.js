2e0f11dd5bdf3c9e55815f8274b476c9c3797576
Tamás Csertán <33261144+csertant@users.noreply.github.com>
2021-02-22 16:32:18 +0100

    ..

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-var default_image = "default.png"
+var default_image = "default.png";
 
-var btn = document.querySelector('.btn-toggle')
+var btn = document.querySelector('.btn-toggle');
+var btn_icon_dark = document.querySelector('#btn-toggle-icon-dark');
+var btn_icon_light = document.querySelector('#btn-toggle-icon-light');
 btn.addEventListener("click", function() {
     // Then toggle (add/remove) the .dark-theme class to the body
     document.body.classList.toggle("dark-theme");
+    btn_icon_dark.classList.toggle("hidden");
+    btn_icon_light.classList.toggle("hidden");
 });
 
 window.onload = displayImage(default_image);
 
 function displayImage(image_slug) {
-    var container = document.getElementById('image-show')
-    container.style.background = "url('./assets/"+ image_slug + "') no-repeat center"
-    container.style.backgroundSize = "cover"
+    var container = document.getElementById('image-show');
+    container.style.background = "url('./assets/"+ image_slug + "') no-repeat center";
+    container.style.backgroundSize = "cover";
 }
